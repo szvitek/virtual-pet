141458b592c8be484742ec5fb0b915243be41ade
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-26 03:03:29 +0000

    feat: dragging sprites

--- js/game.js
+++ js/game.js
@@ -31,10 +31,22 @@ gameScene.preload = function() {
 // create
 gameScene.create = function() {
   // bg
-  this.add.sprite(0, 0, "backyard").setOrigin(0, 0);
+  this.add
+    .sprite(0, 0, "backyard")
+    .setOrigin(0, 0)
+    .setInteractive();
 
   // pet
-  this.pet = this.add.sprite(100, 200, "pet", 0);
+  this.pet = this.add.sprite(100, 200, "pet", 0).setInteractive();
+
+  // make pet draggable
+  this.input.setDraggable(this.pet);
+  // follow pointer (mouse/finger) when dragging
+  this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
+    // make sprite be located at the coordinates of the dragging
+    gameObject.x = dragX;
+    gameObject.y = dragY;
+  });
 
   this.createUi();
 };
@@ -43,9 +55,30 @@ gameScene.create = function() {
 gameScene.createUi = function() {
   // buttons
   this.appleBtn = this.add.sprite(72, 570, "apple").setInteractive();
+  this.appleBtn.customStats = { health: 20, fun: 0 };
+  this.appleBtn.on("pointerdown", this.pickItem);
+
   this.candyBtn = this.add.sprite(144, 570, "candy").setInteractive();
+  this.candyBtn.customStats = { health: -10, fun: 10 };
+  this.candyBtn.on("pointerdown", this.pickItem);
+
   this.toyBtn = this.add.sprite(216, 570, "toy").setInteractive();
+  this.toyBtn.customStats = { health: 0, fun: 15 };
+  this.toyBtn.on("pointerdown", this.pickItem);
+
   this.rotateBtn = this.add.sprite(288, 570, "rotate").setInteractive();
+  this.rotateBtn.on("pointerdown", this.rotatePet);
+};
+
+// rotate pet
+gameScene.rotatePet = function() {
+  console.log("rotating the pet");
+};
+
+// pick item
+gameScene.pickItem = function() {
+  // since this function was passed as an event handler this refers to btn sprites
+  console.log("picking an item %s", this.texture.key);
 };
 
 // update loop
